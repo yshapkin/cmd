3fbb912fb5396a425a41a8f4ca06e0ab05cee3f8
Yury <1420093+yshapkin@users.noreply.github.com>
2022-05-28 22:42:20 +0300

    Create PowerShell_profile.ps1

---
+++ .config/powershell/PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+(@(&"C:/Users/yuryshapkin/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe" init pwsh --config="C:\Users\yuryshapkin\.config\powershell\M365Princess.omp.json" --print) -join "`n") | Invoke-Expression
+
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Alias
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
