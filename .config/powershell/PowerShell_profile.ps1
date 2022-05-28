fd91441f72751eab379c802c54ac3634f5ebda29
Yury <1420093+yshapkin@users.noreply.github.com>
2022-05-29 00:34:13 +0300

    Update PowerShell_profile.ps1

--- .config/powershell/PowerShell_profile.ps1
+++ .config/powershell/PowerShell_profile.ps1
@@ -1,10 +1,15 @@
 # set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-(@(&"C:/Users/<user>/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe" init pwsh --config="C:\Users\<user>\.config\powershell\M365Princess.omp.json" --print) -join "`n") | Invoke-Expression
+(@(&"C:/Users/yuryshapkin/AppData/Local/Programs/oh-my-posh/bin/oh-my-posh.exe" init pwsh --config="C:\Users\yuryshapkin\.config\powershell\M365Princess.omp.json" --print) -join "`n") | Invoke-Expression
 
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
+
+Import-Module -Name Terminal-Icons
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
 # Alias
 Set-Alias g git
